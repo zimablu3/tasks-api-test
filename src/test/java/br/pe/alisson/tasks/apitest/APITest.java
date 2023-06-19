af069eb0cded523d1b8ad2fe13cdda527370a852
zimablu3 <alissonnnunes12@gmail.com>
2023-06-19 09:14:07 -0300

    teste de remocao

--- src/test/java/br/pe/alisson/tasks/apitest/APITest.java
+++ src/test/java/br/pe/alisson/tasks/apitest/APITest.java
@@ -56,6 +56,32 @@ public class APITest {
 		;
 
 	}
+	
+	@Test
+	public void deveRemoverTarefaComSucesso() {
+		//inserir
+		
+		Integer id = RestAssured.given()
+			.body("{\"task\":\"Tarefa para remoção\",	\"dueDate\":\"2023-12-30\"}")
+			.contentType(ContentType.JSON)
+		.when()
+			.post("/todo")
+		.then()
+//			.log().all()
+			.statusCode(201)
+			.extract().path("id")
+		;
+		
+		System.out.println(id);
+		
+		//remover
+		RestAssured.given()
+		.when()
+			.delete("/todo/"+id)
+		.then()
+			.statusCode(204)
+		;
+	}
 
 }
 
